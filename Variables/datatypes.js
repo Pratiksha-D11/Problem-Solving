027849f51cc0a2f8197f88d2d54b667a1697c37d
Pratiksha Deshmukh <dpratiksha741@gmail.com>
2026-07-01 22:43:58 +0530

    Initial commit

---
+++ Variables/datatypes.js
@@ -0,0 +1,47 @@
+// String
+let name = "Pratiksha";
+console.log(name);
+console.log(typeof name);
+
+// Number
+let age = 25;
+console.log(age);
+console.log(typeof age);
+
+// Boolean
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+// Undefined
+let city;
+console.log(city);
+console.log(typeof city);
+
+// Null
+let country = null;
+console.log(country);
+console.log(typeof country); // object (JavaScript quirk)
+
+// Object
+let person = {
+    name: "Pratiksha",
+    age: 25
+};
+console.log(person);
+console.log(typeof person);
+
+// Array
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+console.log(typeof fruits);
+
+// Symbol
+let id = Symbol("id");
+console.log(id);
+console.log(typeof id);
+
+// BigInt
+let bigNumber = 123456789012345678901234567890n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
